2b6b0264a3c135ff309c1a36ec9fcceee359b384
Matheus Felipe Pasold <mfpasold@furb.br>
2019-09-10 20:49:28 -0300

    app atualizacao

--- src/App.java
+++ src/App.java
@@ -13,21 +13,40 @@ import java.util.Scanner;
  */
 public class App {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner teclado = new Scanner(System.in);
         Lutador lutador = new Lutador();
+        int opcao = 1;
         
-            System.out.println("Olá, bem vindo ao nosso sistema, por favor digite o nome do lutador: ");
-            lutador.setNome(teclado.nextLine());
-        try {
-            System.out.println("Agora informe o peso do lutador: ");
-            lutador.setPeso(Integer.parseInt(teclado.nextLine()));
-        }catch(Exception e) {
-            System.out.println("Valor inválido, por favor digite novamente: ");
-            lutador.setPeso(Integer.parseInt(teclado.nextLine()));
+        do {
+            System.out.println("Digite 1 para adicionar um lutador e 2 para ver a característica desse lutador");
+            opcao = Integer.parseInt(teclado.nextLine());
+            
+            switch(opcao) {
+                case 1:
+                    novoLutador(lutador, teclado);
+                    break;
+                case 2:
+                    System.out.println(lutador.imprimir());
+                    break;
+            }
+            
+        }while(opcao == 1 || opcao == 2);
+        
+        System.out.println("Adeus");
+    }
+
+    private static void novoLutador(Lutador lutador, Scanner teclado) throws Exception{
+        System.out.println("Olá, bem vindo ao nosso sistema, por favor digite o nome do lutador: ");
+        lutador.setNome(teclado.nextLine());
+        while(true) {
+            try {
+                System.out.println("Agora informe o peso do lutador: ");
+                lutador.setPeso(Integer.parseInt(teclado.nextLine()));
+                break;
+            }catch(Exception e) {
+                System.out.println("Valor inválido");
+            }
         }
-        
-        System.out.println(lutador.imprimir());
-        
     }
 }
